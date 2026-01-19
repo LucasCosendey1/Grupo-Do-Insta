4abee492927fbf7004fe6a67e4c67d07bc94c88c
Lucas Cosendey <lukecosendey@gmail.com>
2026-01-19 20:04:49 -0300

    Corrigindo erros de UX/UI

--- Tailwindcss.config.js
+++ Tailwindcss.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // Se o projeto estiver na raiz
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    
+    // Se o projeto usar a pasta 'src' (MUITO IMPORTANTE)
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
